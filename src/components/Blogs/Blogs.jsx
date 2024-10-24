b4d09788a50c822388449e887eede00d48b641f3
mehedipro3 <mehediratul37@gmail.com>
2024-10-24 19:05:08 +0600

    blog section added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -6,11 +6,11 @@ const Blogs = () => {
   useEffect(()=>{
     fetch('blogs.json')
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>setBlogs(data))
   },[])
   return (
-    <div>
-      
+    <div className="md:w-2/3">
+      <h2 className="font-bold text-2xl">Blogs:{blogs.length}</h2>
     </div>
   );
 };
